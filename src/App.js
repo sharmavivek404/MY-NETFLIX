391bd405e94a3a316c6e12458616fecac54f67f1
sharmavivek404 <viveksharma.work24@gmail.com>
2026-06-21 23:21:55 +0530

    components are being created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import Body from "./components/Body";
 function App() {
-  return (
-    <div className= "text-3xl text-bold text-yellow-400" >Begining of netflix gpt By vivek </div>
-  );
+  return(
+     <Body/>
+  ); 
 }
 
 export default App;
