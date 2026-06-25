af5f9721ea3600425bab5a4f13ccc5506a2fb317
sharmavivek404 <viveksharma.work24@gmail.com>
2026-06-25 23:08:22 +0530

    login page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
+ import { createBrowserRouter } from "react-router-dom";
  import Login from "./Login";
  import Browse from "./Browse";
- 
+ import {RouterProvider} from "react-router-dom";
  const Body = ()=>{
+
+const appRouter = createBrowserRouter([
+ {
+   path: "/",
+   element: <Login/>
+ },
+ {
+   path:"/browse",
+   element: <Browse />,
+ }
+]
+)
+
     return (
     <div>
-       <Login/>
-       <Browse/>
+       <RouterProvider router={appRouter} />
     </div>
     );
  };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,10 @@
+ import Header from "./Header";
  const Login = ()=>{
-    return <div>Login</div>;
+    return <div>
+      <Header/>
+      <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/81b52f88-dc76-488d-a939-0cf13a260a6e/web/IN-en-20260622-TRIFECTA-perspective_d39d60ef-cb5a-4793-9546-0a8d9a87948e_large.jpg" alt="bg_img"/>
+      </div>
+    </div>;
  };
  export default Login;
